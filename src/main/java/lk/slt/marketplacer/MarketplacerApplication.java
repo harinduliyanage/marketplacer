760276979ee2e0e94de0fac85f6a81858485c6b3
harinduliyanage <harindu.sul@gmail.com>
2023-09-01 16:47:34 +0530

    initial commit

---
+++ src/main/java/lk/slt/marketplacer/MarketplacerApplication.java
@@ -0,0 +1,13 @@
+package lk.slt.marketplacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/slt/marketplacer/MarketplacerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.slt.marketplacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
