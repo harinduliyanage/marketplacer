46a8327b3aa3c2fd6294582ed2d2310861cfc4f5
harinduliyanage <harindu.sul@gmail.com>
2023-09-29 19:15:45 +0530

    system constants file

---
+++ src/main/java/lk/slt/marketplacer/util/Constants.java
@@ -0,0 +1,12 @@
+package lk.slt.marketplacer.util;
+
+public class Constants {
+
+    private Constants() {
+        throw new IllegalStateException("constants utils can not use for making objects");
+    }
+
+    public static final String USR_NOT_FOUND_MSG = "User '%s' not found";
+    public static final String STORE_NOT_FOUND_MSG = "Store '%s' not found of user '%s'";
+
+}
