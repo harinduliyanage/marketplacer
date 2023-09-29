db3da7891bf6333830609019e5d0c75594099351
harinduliyanage <harindu.sul@gmail.com>
2023-09-30 00:26:18 +0530

    updated

--- src/main/java/lk/slt/marketplacer/controller/StoreController.java
+++ src/main/java/lk/slt/marketplacer/controller/StoreController.java
@@ -3,12 +3,10 @@ package lk.slt.marketplacer.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lk.slt.marketplacer.dto.CreateStoreDto;
 import lk.slt.marketplacer.dto.StoreDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "store-services")
+@RequestMapping("/api/v1")
 public interface StoreController {
     @PostMapping(value = "/users/{userId}/stores", consumes = {"application/json"}, produces = {"application/json"})
     public StoreDto createStore(@PathVariable("userId") String userId, @RequestBody CreateStoreDto createStoreDto);
--- src/main/java/lk/slt/marketplacer/controller/UserController.java
+++ src/main/java/lk/slt/marketplacer/controller/UserController.java
@@ -2,11 +2,13 @@ package lk.slt.marketplacer.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author harindu.sul@gmail.com
  */
 @Tag(name = "user-services")
+@RequestMapping("/api/v1")
 public interface UserController {
     @GetMapping(value = "/users", consumes = {"application/json"}, produces = {"application/json"})
     public void getUsers();
