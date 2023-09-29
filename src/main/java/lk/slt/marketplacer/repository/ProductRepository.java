2b91ffb8254067fcdded92d96cb28be0a2a35d3d
harinduliyanage <harindu.sul@gmail.com>
2023-09-29 19:17:28 +0530

    data access implementation

--- src/main/java/lk/slt/marketplacer/repository/ProductRepository.java
+++ src/main/java/lk/slt/marketplacer/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package lk.slt.marketplacer.repository;
 
-import lk.slt.marketplacer.model.User;
+import lk.slt.marketplacer.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
  * @author harindu.sul@gmail.com
  */
 @Repository
-public interface UserRepository extends JpaRepository<User, String>,
-        QuerydslPredicateExecutor<User> {
+public interface ProductRepository extends JpaRepository<Product, String>,
+        QuerydslPredicateExecutor<Product> {
 }
