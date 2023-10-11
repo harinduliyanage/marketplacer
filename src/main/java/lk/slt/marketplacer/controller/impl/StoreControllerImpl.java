1a9254251fb03cd0d84f0c8df89ddbff230896a7
thakshara <thakshara@surge.global>
2023-10-11 17:21:25 +0530

    Create User, Store, SocialLink mappers

--- src/main/java/lk/slt/marketplacer/controller/impl/StoreControllerImpl.java
+++ src/main/java/lk/slt/marketplacer/controller/impl/StoreControllerImpl.java
@@ -4,10 +4,9 @@ import lk.slt.marketplacer.controller.StoreController;
 import lk.slt.marketplacer.dto.CreateStoreDto;
 import lk.slt.marketplacer.dto.ListResponseDto;
 import lk.slt.marketplacer.dto.StoreDto;
+import lk.slt.marketplacer.dto.mapper.StoreMapper;
 import lk.slt.marketplacer.model.Store;
-import lk.slt.marketplacer.model.User;
 import lk.slt.marketplacer.service.StoreService;
-import lk.slt.marketplacer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,27 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StoreControllerImpl implements StoreController {
-
     @Autowired
-    private StoreService storeService;
-    @Autowired
-    private UserService userService;
-
+    StoreService storeService;
     @Override
     public StoreDto createStore(String userId, CreateStoreDto createStoreDto) {
-        User user = userService.getUserById(userId);
-
-        return null;
+        Store store = storeService.createStore(userId,StoreMapper.INSTANCE.createStoreDtoToStore(createStoreDto));
+        return StoreMapper.INSTANCE.storeToStoreDto(store);
     }
 
     @Override
     public ListResponseDto<StoreDto> getStores(String userId, Pageable pageable) {
-        Page<Store> page = this.storeService.getUserStores(userId, pageable);
+        Page<Store> page = storeService.getStores(pageable);
         return ListResponseDto.<StoreDto>builder()
+                .data(StoreMapper.INSTANCE.storeListToStoreDtoList(page.getContent()))
                 .page(pageable.getPageNumber())
                 .limit(pageable.getPageSize())
                 .totalResults(page.getNumberOfElements())
-                //.data(page.getContent())
                 .totalPages(page.getTotalPages())
                 .build();
     }
---
+++ src/main/java/lk/slt/marketplacer/dto/mapper/SocialLinkMapper.java
@@ -0,0 +1,13 @@
+package lk.slt.marketplacer.dto.mapper;
+
+import lk.slt.marketplacer.dto.SocialLinkDto;
+import lk.slt.marketplacer.model.SocialLink;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface SocialLinkMapper {
+    SocialLinkMapper INSTANCE = Mappers.getMapper( SocialLinkMapper.class );
+    SocialLinkDto socialLinkToSocialLinkDto(SocialLink socialLink);
+    SocialLink socialLinkDtoToSocialLink(SocialLinkDto socialLinkDto);
+}
