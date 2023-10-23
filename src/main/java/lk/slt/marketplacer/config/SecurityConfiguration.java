b738e91b8f36b287ef16f247683e2755ffcd0184
thakshara <thakshara@surge.global>
2023-10-23 23:53:31 +0530

    Update Security configuration rules

--- src/main/java/lk/slt/marketplacer/config/SecurityConfiguration.java
+++ src/main/java/lk/slt/marketplacer/config/SecurityConfiguration.java
@@ -31,17 +31,13 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        //Authenticate access
-                        .requestMatchers("api/v1/users").authenticated()
-                        .requestMatchers("api/v1/users/*").authenticated()
-                        .requestMatchers(HttpMethod.POST, "api/v1/users/{userId}/stores/**").authenticated()
-                        .requestMatchers(HttpMethod.PATCH, "api/v1/users/{userId}/stores/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "api/v1/users/{userId}/stores/**").authenticated()
                         //Permit access
                         .requestMatchers("api/docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/products/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/stores/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/users/{userId}/stores/**").permitAll()
+                        //Authenticate access
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2ResourceServer ->
                         oauth2ResourceServer
