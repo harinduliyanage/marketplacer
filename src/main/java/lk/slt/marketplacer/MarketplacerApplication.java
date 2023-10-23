5c1d471305159a82f0865c04980962c88bd326d9
thakshara <thakshara@surge.global>
2023-10-23 23:51:15 +0530

    Remove unused imports

--- src/main/java/lk/slt/marketplacer/MarketplacerApplication.java
+++ src/main/java/lk/slt/marketplacer/MarketplacerApplication.java
@@ -1,18 +1,13 @@
 package lk.slt.marketplacer;
 
-import lk.slt.marketplacer.dto.SocialLinkDto;
-import lk.slt.marketplacer.dto.mapper.SocialLinkMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 
 /**
  * @author harindu.sul@gmail.com
  */
 @SpringBootApplication
-
 public class MarketplacerApplication {
 
 	public static void main(String[] args) {
