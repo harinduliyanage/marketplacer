5635cfa34039f33a2d58c8a99eafa1f5046778d8
thakshara <thakshara@surge.global>
2023-10-25 23:58:32 +0530

    Add @Transactional to resolve product delete error

--- src/main/java/lk/slt/marketplacer/service/impl/ProductServiceImpl.java
+++ src/main/java/lk/slt/marketplacer/service/impl/ProductServiceImpl.java
@@ -14,11 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @Slf4j
+@Transactional
 public class ProductServiceImpl implements ProductService {
     @Autowired
     ProductRepository productRepository;
