d0719313b5ac908caa3ebec20ba1b60b929bbf37
thakshara <thakshara@surge.global>
2023-10-25 16:22:37 +0530

    Store Dto basic validation

--- src/main/java/lk/slt/marketplacer/controller/StoreController.java
+++ src/main/java/lk/slt/marketplacer/controller/StoreController.java
@@ -1,6 +1,7 @@
 package lk.slt.marketplacer.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lk.slt.marketplacer.dto.CreateStoreDto;
 import lk.slt.marketplacer.dto.ListResponseDto;
 import lk.slt.marketplacer.dto.StoreDto;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public interface StoreController {
     @PostMapping(value = "/users/{userId}/stores", consumes = {"application/json"}, produces = {"application/json"})
-    public StoreDto createStore(@PathVariable("userId") String userId, @RequestBody CreateStoreDto createStoreDto);
+    public StoreDto createStore(@PathVariable("userId") String userId, @Valid @RequestBody CreateStoreDto createStoreDto);
 
     @GetMapping(value = "/users/{userId}/stores", produces = {"application/json"})
     public ListResponseDto<StoreDto> getUserStores(@PathVariable("userId") String userId, @ParameterObject Pageable pageable);
@@ -22,7 +23,7 @@ public interface StoreController {
     public StoreDto getStore(@PathVariable("userId") String userId, @PathVariable("storeId") String storeId);
 
     @PatchMapping(value = "/users/{userId}/stores/{storeId}", consumes = {"application/json"}, produces = {"application/json"})
-    public StoreDto updateStore(@PathVariable("userId") String userId, @PathVariable("storeId") String storeId, @RequestBody UpdateStoreDto updateStoreDto);
+    public StoreDto updateStore(@PathVariable("userId") String userId, @PathVariable("storeId") String storeId, @Valid @RequestBody UpdateStoreDto updateStoreDto);
 
     @DeleteMapping(value = "/users/{userId}/stores/{storeId}", produces = {"application/json"})
     public StoreDto removeStore(@PathVariable("userId") String userId, @PathVariable("storeId") String storeId);
