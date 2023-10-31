d0bece2d69c217955547e233fe9e316a8624cd0f
thakshara <thakshara@surge.global>
2023-10-31 09:17:23 +0530

    Create File Service generic methods

--- src/main/java/lk/slt/marketplacer/service/FileService.java
+++ src/main/java/lk/slt/marketplacer/service/FileService.java
@@ -1,4 +1,21 @@
 package lk.slt.marketplacer.service;
 
+import java.net.URL;
+
 public interface FileService {
+    /**
+     * Create a presigned URL for uploading with a PUT request.
+     *
+     * @param fileName - The name of the object.
+     * @return - The presigned URL for an HTTP PUT.
+     */
+    public URL createUploadUrl(String fileName);
+
+    /**
+     * Create a presigned URL for downloading with a GET request.
+     *
+     * @param fileName - The name of the object.
+     * @return - The presigned URL for an HTTP GET.
+     */
+    public URL createDownloadUrl(String fileName);
 }
