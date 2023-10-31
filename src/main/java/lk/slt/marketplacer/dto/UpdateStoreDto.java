c8c45d55a9ea6f2ba9ef8b4aafc37b6c0581dbab
thakshara <thakshara@surge.global>
2023-10-31 09:13:16 +0530

    Add @Data to create getters & setters

--- src/main/java/lk/slt/marketplacer/dto/UpdateStoreDto.java
+++ src/main/java/lk/slt/marketplacer/dto/UpdateStoreDto.java
@@ -1,7 +1,9 @@
 package lk.slt.marketplacer.dto;
 
 import lk.slt.marketplacer.util.StoreStatus;
+import lombok.Data;
 
+@Data
 public class UpdateStoreDto extends CreateStoreDto{
     private StoreStatus storeStatus;
 }
