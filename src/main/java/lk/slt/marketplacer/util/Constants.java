fbdb488b04255638cd4ebbc36f614d51037ed548
thakshara <thakshara@surge.global>
2023-11-06 11:47:43 +0530

    Add category constants

--- src/main/java/lk/slt/marketplacer/util/Constants.java
+++ src/main/java/lk/slt/marketplacer/util/Constants.java
@@ -6,10 +6,12 @@ public class Constants {
         throw new IllegalStateException("constants utils can not use for making objects");
     }
 
-    public static final String USER_NOT_FOUND_MSG = "Username already existing by given '%s'";
+    public static final String USER_NOT_FOUND_MSG = "User '%s' not found";
     public static final String USERNAME_ALREADY_EXISTS = "User '%s' already exists ";
     public static final String STORE_NOT_FOUND_MSG = "Store '%s' not found of user '%s'";
     public static final String STORE_ALREADY_EXISTS = "Store '%s' already exists given name ";
     public static final String PRODUCT_NOT_FOUND_MSG = "Product '%s' not found of store '%s'";
+    public static final String CATEGORY_NOT_FOUND_MSG = "User '%s' not found";
+    public static final String CATEGORY_ALREADY_EXISTS_MSG = "Category '%s' already exists given name ";
 
 }
