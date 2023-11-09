8e0edaa1c5a924715aa46e912079bf7425bd77f1
thakshara <thakshara@surge.global>
2023-11-09 21:51:00 +0530

    S3 connection create in post construct

--- src/main/java/lk/slt/marketplacer/service/impl/S3FileServiceImpl.java
+++ src/main/java/lk/slt/marketplacer/service/impl/S3FileServiceImpl.java
@@ -1,10 +1,13 @@
 package lk.slt.marketplacer.service.impl;
 
 import com.amazonaws.HttpMethod;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import jakarta.annotation.PostConstruct;
 import lk.slt.marketplacer.service.FileService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +19,26 @@ import java.util.Date;
 @Service
 @Slf4j
 public class S3FileServiceImpl implements FileService {
-    @Autowired
     private AmazonS3 amazonS3;
-
+    //
+    @Value("${aws.accessKey}")
+    private String accessKey;
+    @Value("${aws.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
     @Value("${aws.s3.bucket}")
     private String bucketName;
+    //
+    @PostConstruct
+    private void init() {
+        final BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        this.amazonS3 = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
 
     /**
      * Create a presigned URL for uploading with a PUT request.
@@ -36,7 +54,7 @@ public class S3FileServiceImpl implements FileService {
         URL presignedUrl = amazonS3.generatePresignedUrl(bucketName, filePath, calendar.getTime(), HttpMethod.PUT);
         log.info("Presigned URL to upload a file to: [{}]", presignedUrl.toString());
         //
-        return  presignedUrl;
+        return presignedUrl;
     }
 
     /**
@@ -53,6 +71,6 @@ public class S3FileServiceImpl implements FileService {
         URL presignedUrl = amazonS3.generatePresignedUrl(bucketName, filePath, calendar.getTime(), HttpMethod.GET);
         log.info("Presigned URL to download a file to: [{}]", presignedUrl.toString());
         //
-        return  presignedUrl;
+        return presignedUrl;
     }
 }
