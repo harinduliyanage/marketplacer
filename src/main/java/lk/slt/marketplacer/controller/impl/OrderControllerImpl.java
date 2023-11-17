0422647203002880a38a2951205245a082ab573d
thakshara <thakshara@surge.global>
2023-11-17 22:40:15 +0530

    Create Order Controller

---
+++ src/main/java/lk/slt/marketplacer/controller/impl/OrderControllerImpl.java
@@ -0,0 +1,61 @@
+package lk.slt.marketplacer.controller.impl;
+
+import lk.slt.marketplacer.controller.OrderController;
+import lk.slt.marketplacer.dto.*;
+import lk.slt.marketplacer.dto.mapper.OrderMapper;
+import lk.slt.marketplacer.model.Order;
+import lk.slt.marketplacer.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerImpl implements OrderController {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderMapper orderMapper;
+
+    @Override
+    public OrderDto createOrder(String userId, CreateOrderDto createOrderDto) {
+        return orderMapper.orderToOrderDto(
+                orderService.createOrder(userId, orderMapper
+                        .createOrderDtoToOrder(createOrderDto)
+                )
+        );
+    }
+
+    @Override
+    public ListResponseDto<OrderDto> getOrders(String userId, Pageable pageable) {
+        Page<Order> page = orderService.getOrders(userId, pageable);
+        return ListResponseDto.<OrderDto>builder()
+                .data(orderMapper.orderListToOrderDtoList(page.getContent()))
+                .page(pageable.getPageNumber())
+                .limit(pageable.getPageSize())
+                .totalResults(page.getNumberOfElements())
+                .totalPages(page.getTotalPages())
+                .build();
+    }
+
+    @Override
+    public OrderDto getOrder(String userId, String orderId) {
+        return orderMapper.orderToOrderDto(orderService.getOrder(userId, orderId));
+    }
+
+    @Override
+    public OrderDto updateOrder(String userId, String orderId, UpdateOrderDto updateOrderDto) {
+        return orderMapper.orderToOrderDto(orderService
+                .updateOrder(orderId,
+                        orderMapper.updateOrderDtoToOrder(updateOrderDto,
+                                orderService.getOrder(userId, orderId)
+                        )
+                )
+        );
+    }
+
+    @Override
+    public OrderDto removeOrder(String userId, String orderId) {
+        return orderMapper.orderToOrderDto(orderService.removeOrder(userId, orderId));
+    }
+}
