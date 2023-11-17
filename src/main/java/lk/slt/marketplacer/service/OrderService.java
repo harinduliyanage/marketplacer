301c4d1221702a12aee6597238666e9f9eaa0cb8
thakshara <thakshara@surge.global>
2023-11-17 22:39:45 +0530

    Create Order Service

---
+++ src/main/java/lk/slt/marketplacer/service/OrderService.java
@@ -0,0 +1,18 @@
+package lk.slt.marketplacer.service;
+
+import lk.slt.marketplacer.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface OrderService {
+    public Order createOrder(String userId,  Order order);
+
+    public Page<Order> getOrders(String userId, Pageable pageable);
+
+    public Order getOrder(String userId, String orderId);
+
+    public Order updateOrder(String orderId, Order order);
+
+    public Order removeOrder(String userId, String orderId);
+
+}
