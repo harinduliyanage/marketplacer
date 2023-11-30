328c3950d3adb3ee166e58be6cdc7b9df07b1929
harinduliyanage <harindu.sul@gmail.com>
2023-11-30 16:54:49 +0530

    removed context load

--- src/test/java/lk/slt/marketplacer/MarketplacerApplicationTests.java
+++ src/test/java/lk/slt/marketplacer/MarketplacerApplicationTests.java
@@ -1,13 +1,9 @@
 package lk.slt.marketplacer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MarketplacerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
