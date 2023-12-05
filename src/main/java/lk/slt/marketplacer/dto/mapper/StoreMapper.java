6c9bfb91b09fa12400b365b0956be296d25c4439
harinduliyanage <harindu.sul@gmail.com>
2023-12-05 14:30:26 +0530

    remove unuse import

--- src/main/java/lk/slt/marketplacer/dto/mapper/StoreMapper.java
+++ src/main/java/lk/slt/marketplacer/dto/mapper/StoreMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
