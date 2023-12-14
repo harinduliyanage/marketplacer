37fe152a40edcf726a45742bbc2ff000981af03b
harinduliyanage <harindu.sul@gmail.com>
2023-12-14 13:35:48 +0530

    removed unused import

--- src/main/java/lk/slt/marketplacer/dto/CreateReviewDto.java
+++ src/main/java/lk/slt/marketplacer/dto/CreateReviewDto.java
@@ -1,6 +1,5 @@
 package lk.slt.marketplacer.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
