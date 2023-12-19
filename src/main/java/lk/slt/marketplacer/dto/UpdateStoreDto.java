1e4f86f836e9a858a3be255d2ad659b9a1951489
thakshara <thakshara@surge.global>
2023-12-19 16:16:54 +0530

    Fix: Dto Mappers warnings

--- src/main/java/lk/slt/marketplacer/dto/UpdateStoreDto.java
+++ src/main/java/lk/slt/marketplacer/dto/UpdateStoreDto.java
@@ -1,9 +1,34 @@
 package lk.slt.marketplacer.dto;
 
+import jakarta.validation.constraints.Email;
 import lk.slt.marketplacer.util.StoreStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
+import java.util.List;
 
 @Data
-public class UpdateStoreDto extends CreateStoreDto{
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateStoreDto{
+    private List<SocialLinkDto> socialLinks;
+    //
+    private String name;
+    private String description;
+    private String categoryId;
+    //
+    private String telephone;
+    private String fax;
+    private String address;
+    private String brFilePath;
+    private String logoFilePath;
+    @Email
+    private String email;
+    @URL
+    private String website;
     private StoreStatus storeStatus;
 }
