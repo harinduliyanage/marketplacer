a82939899c9aea192b32fffc90f88a6fff72fb08
Nipuna Akalana <nipuna@surge.global>
2024-02-10 14:56:13 +0530

    Missing import

--- src/main/java/lk/slt/marketplacer/service/impl/StoreServiceImpl.java
+++ src/main/java/lk/slt/marketplacer/service/impl/StoreServiceImpl.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
